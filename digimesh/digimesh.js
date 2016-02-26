c2f26a0e31e4a26fcd318d066b1a2499c7a8422e
lpercifield@gmail.com <lpercifield@gmail.com>
2016-02-26 18:24:27 -0500

    trying exit

--- digimesh/digimesh.js
+++ digimesh/digimesh.js
@@ -151,7 +151,8 @@ var XbeeDigiMesh = function(config, callback) {
             num_complete++; if (num_complete === 3) complete();
         });
     });
-    this.exit = function() { 
+    this.exit = function() {
+      console.log("calling exit")
       this.serial_port.close(function(err) {
         if (err) console.log("Error closing port: "+util.inspect(err));
         process.exit();
