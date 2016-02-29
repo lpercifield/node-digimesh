607d064ee3f7e24c35b333ffafe7501c8f777dc5
lpercifield@gmail.com <lpercifield@gmail.com>
2016-02-29 13:39:08 -0500

    handle payload better

--- link-nodes/556-digimesh.js
+++ link-nodes/556-digimesh.js
@@ -142,9 +142,9 @@ var xbee;
       xbee.on('message_received', function(data) {
         //var hexout = hex_to_ascii(data.data);
         console.log('received a message from %s with data: %s', data.addr,data.data);
-        var obj = {"payload": JSON.parse(data.data)};
-        obj.payload["source"]= data.addr;
-        node.send({ "payload": obj, "source": data.addr });
+        var obj = {"data": JSON.parse(data.data)};
+        //obj.payload["source"]= data.addr;
+        node.send({ "data": obj, "source": data.addr });
         // var length = data.data.length;
         // var outstring = "";
         // switch (length) {
