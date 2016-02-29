650567bc418e74c6f24c6d855f92b816798f9d8c
lpercifield@gmail.com <lpercifield@gmail.com>
2016-02-28 21:09:19 -0500

    trying sp

--- link-nodes/556-digimesh.js
+++ link-nodes/556-digimesh.js
@@ -59,7 +59,7 @@ var xbee;
                   });
                   console.log("DigimeshInNode closed");
               });
-              var buf = new Buffer("1170");
+              var buf = new Buffer("1170","hex");
               xbee.at_command("SP",buf,function(){
                 console.log("done with sp");
               })
